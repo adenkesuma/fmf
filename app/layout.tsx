a78281266709eb57ba52d2ef627ccecb57275bd5
adenkesuma <adenfdfd10@gmail.com>
2025-01-16 12:03:48 +0700

    `fix:` delete container on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,9 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        <div className="container mx-auto">
-          {children}
-        </div>
+        {children}
       </body>
     </html>
   );
