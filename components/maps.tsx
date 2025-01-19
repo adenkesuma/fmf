39436c9727cc25b170cb6263670039f6d1aa5a5c
adenkesuma <adenfdfd10@gmail.com>
2025-01-19 16:06:57 +0700

    `fix:` add if condition

--- components/maps.tsx
+++ components/maps.tsx
@@ -1,38 +1,3 @@
-// "use client"
-
-// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-// import "leaflet/dist/leaflet.css";
-// import L from "leaflet";
-
-// // Menambahkan icon marker agar sesuai dengan peta Leaflet
-// const customIcon = new L.Icon({
-//   iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-//   iconSize: [25, 41],
-//   iconAnchor: [12, 41],
-// });
-
-// const Maps = () => {
-//   if (typeof window === "undefined") return null;
-
-//   const monasPosition = [-6.175392, 106.827153]; 
-
-//   return (
-//     <div className="w-full h-[500px] rounded-xl">
-//       <MapContainer center={L.latLng(monasPosition[0], monasPosition[1])} zoom={16} className="w-full h-full">
-//         <TileLayer
-//           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//         />
-//         <Marker position={L.latLng(monasPosition[0], monasPosition[1])} icon={customIcon}>
-//           <Popup>Monumen Nasional (Monas), Jakarta</Popup>
-//         </Marker>
-//       </MapContainer>
-//     </div>
-//   );
-// };
-
-// export default Maps;
-
 "use client";
 
 import { useEffect, useState } from "react";
@@ -45,6 +10,8 @@ let L: typeof import("leaflet"); // Deklarasi tipe untuk Leaflet
 const Maps = () => {
   const [customIcon, setCustomIcon] = useState<L.Icon | null>(null); // Berikan tipe untuk customIcon
   const monasPosition = [-6.175392, 106.827153];
+
+  if (typeof window === "undefined") return null;
 
   useEffect(() => {
     (async () => {
