7d09ab6076a810a18f6dd2b9afe7cb9d97494336
adenkesuma <adenfdfd10@gmail.com>
2025-01-22 22:50:23 +0700

    fix navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -86,30 +86,32 @@ export default function Navbar() {
         <Link className="text-sm font-medium text-white hidden lg:block" href={'/'}>Home</Link>
 
         {/* hamburger menu */}
-        <DropdownMenu>
-          <DropdownMenuTrigger className="outline-none border-none">
-            <div>
-              <svg data-slot="icon" fill="none" strokeWidth="1.5" className="size-6 text-white" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"></path>
-              </svg>
-            </div>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="bg-primary-color border-white/20 text-white flex flex-col p-2">
-            <Link href={'/'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Halaman Utama</Link>
-            <Link href={'/welcome-message'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Pesan Selamat Datang</Link>
-            <Link href={'/overview'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Ringkasan</Link>
-            <Link href={'/organizing-commitee'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Panitia Penyelenggara</Link>
-            <Link href={'/program-at-glance'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Sekilas Tentang Program</Link> 
-            <Link href={'/invited-speakers'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Pembicara Yang Di Undang</Link>
-            <Link href={'/registration'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Pendaftaran</Link>
-            <Link href={'/sponsorship'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Sponsor</Link>
-            <Link href={'/accomodations'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Akomodasi</Link>
-            <Link href={'/venue'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Lokasi</Link>
-            <Link href={'/jakarta-indonesia'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Jakarta, Indonesia</Link>
-            <Link href={'/transportation'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Transportasi</Link>
-            <Link href={'/general-information'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Informasi Umum</Link>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <div className="block lg:hidden">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="outline-none border-none">
+              <div>
+                <svg data-slot="icon" fill="none" strokeWidth="1.5" className="size-6 text-white" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"></path>
+                </svg>
+              </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="bg-primary-color border-white/20 text-white flex flex-col p-2">
+              <Link href={'/'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Halaman Utama</Link>
+              <Link href={'/welcome-message'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Pesan Selamat Datang</Link>
+              <Link href={'/overview'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Ringkasan</Link>
+              <Link href={'/organizing-commitee'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Panitia Penyelenggara</Link>
+              <Link href={'/program-at-glance'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Sekilas Tentang Program</Link> 
+              <Link href={'/invited-speakers'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Pembicara Yang Di Undang</Link>
+              <Link href={'/registration'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Pendaftaran</Link>
+              <Link href={'/sponsorship'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Sponsor</Link>
+              <Link href={'/accomodations'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Akomodasi</Link>
+              <Link href={'/venue'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Lokasi</Link>
+              <Link href={'/jakarta-indonesia'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Jakarta, Indonesia</Link>
+              <Link href={'/transportation'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Transportasi</Link>
+              <Link href={'/general-information'} className="hover:bg-white/20 p-2 rounded-lg text-sm font-medium">Informasi Umum</Link>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
 
       </div>
     </div>
