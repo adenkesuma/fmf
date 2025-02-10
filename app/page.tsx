b912a70149c731e137ede381769c7db0dda5bff7
adenkesuma <adenfdfd10@gmail.com>
2025-02-10 19:21:04 +0700

    `fix:` something

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
             <h2 className="font-medium text-2xl text-primary-color">Important Date</h2>
             <div className="w-full grid grid-cols-2 mt-2 gap-4">
               <div className="flex items-center justify-center flex-col border border-primary-color rounded-xl p-7">
-                <div className="flex items-center justify-center bg-yellow-200 w-24 h-24 rounded-full">
+                <div className="flex items-center justify-center bg-yellow-400 w-24 h-24 rounded-full">
                   <svg className="size-16 text-primary-color" data-slot="icon" fill="none" strokeWidth="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
                   </svg>
@@ -28,7 +28,7 @@ export default function Home() {
                 <p className="text-base text-center text-primary-color">20 Januari 2025</p>
               </div>
               <div className="flex items-center justify-center flex-col border border-primary-color rounded-xl p-7">
-                <div className="flex items-center justify-center bg-yellow-200 w-24 h-24 rounded-full">
+                <div className="flex items-center justify-center bg-yellow-400 w-24 h-24 rounded-full">
                   <svg className="size-16 text-primary-color" data-slot="icon" fill="none" strokeWidth="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"></path>
                   </svg>
