02f3470cff54f4d1cbe24f41cebdbe1a75c97b36
adenkesuma <adenfdfd10@gmail.com>
2025-02-10 20:11:19 +0700

    `feat:` change banner

--- components/banner.tsx
+++ components/banner.tsx
@@ -8,8 +8,8 @@ import BannerImage from '@/public/images/banner.png';
 const Banner = () => {
    return (
     <div className="h-screen relative">
-      <Image src={BannerImage} alt="banner image" className="w-full h-[85%] bg-cover object-cover" />
-      <div className="mb-3 container px-10 absolute left-1/2 -translate-x-1/2 -bottom-14 p-6 flex items-center gap-2 justify-center">
+      <Image src={BannerImage} alt="banner image" className="w-full h-[90%] bg-cover object-cover" />
+      <div className="mb-3 container px-10 absolute left-1/2 -translate-x-1/2 -bottom-16 p-6 flex items-center gap-2 justify-center">
         <div className="bg-white p-2 md:p-4 flex flex-col md:flex-row items-center gap-2 rounded-2xl">
           <CountdownCard eventDate="2025-07-01T00:00:00Z"/>
           <div className="grid grid-cols-2 gap-2">
