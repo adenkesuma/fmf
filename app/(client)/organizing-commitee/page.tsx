041bb68e053e994a680a38d8bf77fdb1a42c9f8d
adenkesuma <adenfdfd10@gmail.com>
2025-03-12 21:42:05 +0700

    `fix:` finish organizing commite

--- app/(client)/organizing-commitee/page.tsx
+++ app/(client)/organizing-commitee/page.tsx
@@ -1,5 +1,5 @@
 import BannerTitle from "@/components/banner-title"
-import { organizingCommittee, commissionA, commissionB, commissionC, commissionD, commissionE } from "@/const/organizing-commite"
+import { organizingCommittee,seminar, workshop, posterAndOralPresentation, familyMedicinaAward, publicRelationsAndPublicationSection, sponsorship, itSection, consumptionSection, secretariat } from "@/const/organizing-commite"
 
 export default function OrganizingCommitee() {
   return (
@@ -10,7 +10,7 @@ export default function OrganizingCommitee() {
         <div className="flex flex-col border-t-4 border-primary-color">
           {organizingCommittee.map((commitee, i) => (
             <div key={commitee.nama} className={`flex items-center border-x border-t ${organizingCommittee.length - 1 === i ? 'border-b' : ''}`}>
-              <div className="bg-blue-500/10 block text-sm w-1/3 p-2">
+              <div className="block text-sm w-1/3 h-inherit shrink-0 p-2">
                 <span className="font-medium">{commitee.jabatan}</span>
               </div>
               <div className="block text-sm p-2 border-l">
@@ -19,37 +19,61 @@ export default function OrganizingCommitee() {
             </div>
           ))}
         </div> 
+        <h2 className="mt-8 font-medium text-2xl mb-2 text-primary-color">
+          Scientific section
+        </h2>
         <div className="mt-6">
-          <h2 className="font-medium text-2xl mb-2 text-primary-color">
-            Commissions
-          </h2>
-          <div className="font-semibold">Commision A: Articles of association and bylaws</div>
-          {commissionA.map((a, idx) => (
-            <div className="pl-1 mt-1" key={idx+a}>{idx+1}. {a}</div>
+          <div className="font-semibold">Seminar</div>
+          {seminar.map((b, idx) => (
+            <div className="pl-1 mt-1 text-sm" key={idx+b}>{idx+1}. {b}</div>
           ))}
         </div>
         <div className="mt-6">
-          <div className="font-semibold">Commision B: Continuous Professional Development Program (P2KB)</div>
-          {commissionB.map((b, idx) => (
-            <div className="pl-1 mt-1" key={idx+b}>{idx+1}. {b}</div>
+          <div className="font-semibold">Workshop</div>
+          {workshop.map((c, idx) => (
+            <div className="pl-1 mt-1 text-sm" key={idx+c}>{idx+1}. {c}</div>
           ))}
         </div>
         <div className="mt-6">
-          <div className="font-semibold">Commision C: Professional Services</div>
-          {commissionC.map((c, idx) => (
-            <div className="pl-1 mt-1" key={idx+c}>{idx+1}. {c}</div>
+          <div className="font-semibold">Poster And Oral Presentation</div>
+          {posterAndOralPresentation.map((d, idx) => (
+            <div className="pl-1 mt-1 text-sm" key={idx+d}>{idx+1}. {d}</div>
           ))}
         </div>
         <div className="mt-6">
-          <div className="font-semibold">Commision D: Etika, Disiplin, dan Hukum</div>
-          {commissionD.map((d, idx) => (
-            <div className="pl-1 mt-1" key={idx+d}>{idx+1}. {d}</div>
+          <div className="font-semibold">Family Medicin Award</div>
+          {familyMedicinaAward.map((e, idx) => (
+            <div className="pl-1 mt-1 text-sm" key={idx+e}>{idx+1}. {e}</div>
           ))}
         </div>
         <div className="mt-6">
-          <div className="font-semibold">Commision E: Organizational Policy and Discussion of the Main Program Outline/ RENSTRA</div>
-          {commissionE.map((e, idx) => (
-            <div className="pl-1 mt-1" key={idx+e}>{idx+1}. {e}</div>
+          <div className="font-semibold">Public Relations and Publication Section</div>
+          {publicRelationsAndPublicationSection.map((b, idx) => (
+            <div className="pl-1 mt-1 text-sm" key={idx+b}>{idx+1}. {b}</div>
+          ))}
+        </div>
+        <div className="mt-6">
+          <div className="font-semibold">Sponsorship</div>
+          {sponsorship.map((c, idx) => (
+            <div className="pl-1 mt-1 text-sm" key={idx+c}>{idx+1}. {c}</div>
+          ))}
+        </div>
+        <div className="mt-6">
+          <div className="font-semibold">It Section</div>
+          {itSection.map((d, idx) => (
+            <div className="pl-1 mt-1 text-sm" key={idx+d}>{idx+1}. {d}</div>
+          ))}
+        </div>
+        <div className="mt-6">
+          <div className="font-semibold">Consumption Section</div>
+          {consumptionSection.map((e, idx) => (
+            <div className="pl-1 mt-1 text-sm" key={idx+e}>{idx+1}. {e}</div>
+          ))}
+        </div>
+         <div className="mt-6">
+          <div className="font-semibold">Secretariat</div>
+          {secretariat.map((e, idx) => (
+            <div className="pl-1 mt-1 text-sm" key={idx+e}>{idx+1}. {e}</div>
           ))}
         </div>
       </div>
--- const/organizing-commite.ts
+++ const/organizing-commite.ts
@@ -1,12 +1,13 @@
 export const organizingCommittee = [
-    { nama: 'dr. Nuzelly Husnedi, MARS, SpKKLP', jabatan: 'Chairman'},
-    { nama: 'dr. Ihsan, Husain, M.Kes, SpKKLP', jabatan: 'Vice Chairman'},
-    { nama: 'dr. Nita Yulinda', jabatan: 'Secretary'},
-    { nama: 'Dr. dr. Fitria Saftarina, MSc, SpKKLP', jabatan: 'Deputy Secretary'},
-    { nama: 'Dr. dr. Maria Selvester Thadeus, M.Biomed, SpKKLP, Subsp.FOMC', jabatan: 'Treasurer'},
-    { nama: 'dr. Yanti Harjono, MKM, SpKKLP Subsp. COPC', jabatan: 'Deputy Treasurer'},
-    { nama: 'Mujiah Hartono', jabatan: 'Secretariat'},
-    { nama: 'Qori Nurul Hasanah Harahap', jabatan: 'Secretariat'},
+    { nama: 'Dr. dr. Isti Ilmiati Fujiati, MSc, CM-FM, MPd.Ked, SpKKLP,Subsp.COPC ', jabatan: 'Person Responsible'},
+    { nama: 'Dr. dr. Judilherry Justam, MM,ME, Dr. dr. AndiArmyn Nurdin, MSc, SpKKLP, Dr. dr. Widyastuti Wibisana, MSc(PH), dr. Sugito Wonodirekso, MS, PHK, PKK, SpKKLP, Prof. (HC), dr. Mariatul Fadillah, MARS, PhD, SpKKLP, Subsp. COPC, Prof. Dr. dr. Sri Andarini, M.Kes, SpKKLP, Subsp.FOMC', jabatan: 'Advisor'},
+    { nama: 'Dr. dr. Farida Rusnianah, M.Kes, SpKKLP, Subsp. FOMC, dr. Trevino Aristarkus Pakasi, FS, MS, PhD, SpKKLP, Subsp.FOMC, dr. Syarhan, MM, SpKKLP ', jabatan: 'Director'},
+    { nama: 'dr. Novana Perdana Putri, SpKKLP', jabatan: 'Chairman'},
+    { nama: 'Dr. dr. Yetty Machrina, M. Kes, AIFO-K., Sp.KKLP, Subsp. FOMC', jabatan: 'Vice Chairman'},
+    { nama: 'dr. Hadiyanto Usman, MKM, SpKKLP, Subsp. FOMC ', jabatan: 'Secretary'},
+    { nama: 'Dr. dr. Maria Selvester Thadeus, M.Biomed, SpKKLP, Subs.FOMC ', jabatan: 'Treasurer'},
+    { nama: 'Dr. dr. Yanti Harjono, MKM, Subsp. COPC', jabatan: 'Deputy Treasurer'},
+    { nama: 'dr. Dewy Mismarita M, MKM, SpKKLP, dr. Niken Harliseptiani Kusumawati, SpKKLP, dr. Hikmah Yanti Arif, SpKKLP', jabatan: 'Events & Equipment Section'},
     
 ];
 
@@ -31,33 +32,60 @@ export const commissionB = [
   'dr. Putu Aryani, MIH, SpKKLP, Subsp. FOMC'
 ];
 
-export const commissionC = [
-  'Dr. dr. Insi Farisa Desy, MSi, SpKKLP, Subsp. FOMC (Koordinator)',
+export const posterAndOralPresentation = [
+  'dr Fitriana Murriya Ekawati, MPHC, SpKKLP, SubSp FOMC, PhD',
+  'dr. Ichsan, MSc, SpKKLP, Subsp. FOMC',
+  'Prof. Dr.dr. Rina Amelia, MARS, Sp.KKLP, Subsp. FOMC',
+  'dr. Putu Aryani, MIH, SpKKLP, Subsp. FOMC',
+  'dr. Yusnita. M.Kes, SpKKLP',
+  'Dr. dr. Insi Farisa Desy, MSi, SpKKLP, Subsp. FOMC',
+  'dr. Nuretha Hevy Purwaningtyas, MSc, SpKKLP, Subsp. FOMC',
+  'Dr. dr. Dian Kusuma Dewi, M.Gizi, SpKKLP, Subsp. FOMC' 
+];
+
+export const seminar = [
+  'dr Ninik Setya Hastuti, M.Far, M.Biomed, AAM,SpKKLP',
+  'dr. Muhammad Arvid Suhada, SpKKLP',
+  'dr. Johan Mardansyah, MKKK, SpKKLP'
+]
+
+export const workshop = [
+  'Dr.dr. Aila Karyus, M.Kes., Sp.KKLP',
+  'dr. Erwilly Frida Putri Astina, M.Kes, SpKKLP',
+  'dr. Yoane Lisa, Sp.KKLP, AIFO-K'  
+]
+
+export const familyMedicinaAward = [
+  'dr.Mustakim, M.Kes, SpKKLP',
+  'dr. Marshell Timotius Handoko, MM, SpKKLP, Subsp.FOMC',
+  'dr. dr. Elda Nazriati, M.Kes, Sp.KKLP, Subsp. FOMC'
+]
+
+export const publicRelationsAndPublicationSection = [
   'dr. Syarhan, MM, SpKKLP',
-  'dr. Trevino Aristarkus Pakasi, FS, MS, PhD, SpKKLP, Subsp.FOMC',
-  'dr. Hendriyanto, MARS, SpKKLP',
-  'dr. Sutriso, MKes, SpKKLP',
-  'dr. Yohanes, MSi, SpKKLP',
-  'dr. Susi Oktowaty, MKM, SpKKLP, Subsp. FOMC',
-  'dr. Hildafitri, Sp.KKLP'
+  'dr. Retno Sari Dewi, M. Kes, AIFO-K, SpKKLP',
+  'dr. V Dwi Jani Juliawati, M.Pd, SpKKLP'
+]
+
+export const sponsorship = [
+  'Dr. dr. Tan Suyono, MARS, M.Biomed, SpKKLP',
+  'Dr. Yetty Rohayati, SpKKLP',
+  'dr. Susi Oktowaty, MKM, SpKKLP, Subsp. FOMC'
+]
+
+export const itSection = [
+  'dr. Edi Alpino, Rivai Siregar, MKK, SpKKLP',
+  'dr. Danny Pattiwarajane, S.Mat, SpKKLP',
+  'dr. Asmani.,M.A.R.S., Sp.KKLP' 
 ];
 
-export const commissionD = [
-  'Dr. dr. Andi Armyn Nurdin, MSc, SpKKLP (Koordinator)',
-  'dr. Novana Perdana Putri, SpKKLP',
-  'Prof. Dr. dr. M. Yani, M.Kes, PKK, SpKKLP, Subs. FOMC',
-  'Dr. dr. Aila Karyus, MSc, SpKKLP',
-  'dr. Mustakim, M.Kes, SpKKLP',
-  'dr. Ade Kurniawan, MH, Kes, Sp.KKLP',
-  'dr. Asmani, M.A.R.S., Sp.KKLP'
+export const consumptionSection = [
+  'dr. Rr. Indira Dewi, SpKKLP',
+  'dr. Camelia Chitra, SpKKLP',
+  'dr. Irma Wina Nasution, SpKKLP'
 ];
 
-export const commissionE = [
-  'Dr. dr. Farida Rusnianah, M.Kes, SpKKLP, Subsp. FOMC (Koordinator)',
-  'dr. Agil Priambodo, M.Biomed, SpKKLP',
-  'dr. Trisnawarman, M.Kes, SpKKLP, Subsp. FOMC',
-  'dr. Junida Sinulingga, M.Kes, SpKKLP',
-  'Dr. dr. Billy Johnson Kepel, M.Med.Sc, Sp.KKLP',
-  'Dr. dr. Ibrahim, MSc, MPd.Ked, SpKKLP',
-  'dr. Andi Rivai Pakki, SpKKLP'
+export const secretariat = [
+  'Mujiah Hartono',
+  'Qori Nurul Hasanah Harahap'
 ];
